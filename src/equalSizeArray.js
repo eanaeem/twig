7e2375363c5bc9c3ca67dcb5b6052885d4416468
eanaeem <eanaeem@gmail.com>
2021-01-22 00:37:02 +0000

    Updated scenario

--- src/equalSizeArray.js
+++ src/equalSizeArray.js
@@ -1,18 +1,42 @@
 /**
  * Divide an array into equally sized arrays
  * @param originalArray - Array to divide
- * @param size - Size of each divided array
+ * @param subArrayNumber - Number of subArrays
  */
 
-function equalSizeArray(originalArray, size) {
-  if (typeof size !== "number" || size < 0) {
-    return "Please provide positive number";
+function equalSizeArray(originalArray, subArrayNumber) {
+  if (
+    !originalArray.length ||
+    typeof subArrayNumber !== "number" ||
+    subArrayNumber < 0
+  ) {
+    return "Please provide non empty array and positive number";
+  }
+
+  const arrLength = originalArray.length;
+
+  if (subArrayNumber === 1 || subArrayNumber > arrLength) {
+    return [originalArray];
   }
 
   let clonedArray = [...originalArray];
-  let finalArray = [];
-  while (clonedArray.length) {
-    finalArray = [...finalArray, clonedArray.splice(0, size)];
+  const finalArray = [];
+  let subArraySize = Math.floor(arrLength / subArrayNumber);
+
+  if (arrLength % subArrayNumber === 0) {
+    while (clonedArray.length) {
+      finalArray.push(clonedArray.splice(0, subArraySize));
+    }
+  } else {
+    let index = 0;
+    subArrayNumber--;
+    subArraySize = Math.floor(arrLength / subArrayNumber);
+    if (arrLength % subArraySize === 0) subArraySize--;
+    while (index < subArraySize * subArrayNumber) {
+      finalArray.push(clonedArray.splice(0, subArraySize));
+      index += subArraySize;
+    }
+    finalArray.push(clonedArray);
   }
 
   return finalArray;
--- test/equalSizeArray.spec.js
+++ test/equalSizeArray.spec.js
@@ -7,23 +7,39 @@ describe("equalSizeArray ", () => {
       [3, 4],
       [5, 6],
     ];
-    expect(equalSizeArray([1, 2, 3, 4, 5, 6], 2)).toEqual(expectedResult);
+    expect(equalSizeArray([1, 2, 3, 4, 5, 6], 3)).toEqual(expectedResult);
   });
-  test(" should return equal size arrays expect last array if not divided equally ", () => {
-    const expectedResult = [[1, 2], [3, 4], [5]];
-    expect(equalSizeArray([1, 2, 3, 4, 5], 2)).toEqual(expectedResult);
+
+  test("should return equal size arrays expect last array if not divided equally ", () => {
+    const expectedResult = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [10, 11, 12],
+      [13],
+    ];
+    expect(
+      equalSizeArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5)
+    ).toEqual(expectedResult);
+  });
+  test(" should return message if array size is not greater than 0 ", () => {
+    expect(equalSizeArray([], 2)).toEqual(
+      "Please provide non empty array and positive number"
+    );
   });
   test(" should return message if size is not a number ", () => {
     expect(equalSizeArray([1, 2, 3, 4, 5], "2")).toEqual(
-      "Please provide positive number"
+      "Please provide non empty array and positive number"
     );
   });
   test(" should return message if size is not positive number ", () => {
     expect(equalSizeArray([1, 2, 3, 4, 5], -2)).toEqual(
-      "Please provide positive number"
+      "Please provide non empty array and positive number"
     );
   });
   test(" should return empty array if empty array is provided ", () => {
-    expect(equalSizeArray([], 2)).toEqual([]);
+    expect(equalSizeArray([], 2)).toEqual(
+      "Please provide non empty array and positive number"
+    );
   });
 });
